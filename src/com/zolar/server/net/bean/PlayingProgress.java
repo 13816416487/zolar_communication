28998ecb7d0e400a206af3c473076d5a9a618f86
zongh <zongh@LAPTOP-8E1U9OLG>
2018-05-11 14:19:27 +0800

    PlayingProgress 的修改

--- src/com/zolar/server/net/bean/PlayingProgress.java
+++ src/com/zolar/server/net/bean/PlayingProgress.java
@@ -22,62 +22,6 @@ public class PlayingProgress {
 
     public boolean uploadSuccess = false;       // 是否提交后台
 
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public int getAccountId() {
-		return accountId;
-	}
-
-	public void setAccountId(int accountId) {
-		this.accountId = accountId;
-	}
-
-	public int getMediaId() {
-		return mediaId;
-	}
-
-	public void setMediaId(int mediaId) {
-		this.mediaId = mediaId;
-	}
-
-	public int getEpisodeId() {
-		return episodeId;
-	}
-
-	public void setEpisodeId(int episodeId) {
-		this.episodeId = episodeId;
-	}
-
-	public int getProgress() {
-		return progress;
-	}
-
-	public void setProgress(int progress) {
-		this.progress = progress;
-	}
-
-	public long getProgressUpdateTime() {
-		return progressUpdateTime;
-	}
-
-	public void setProgressUpdateTime(long progressUpdateTime) {
-		this.progressUpdateTime = progressUpdateTime;
-	}
-
-	public boolean isUploadSuccess() {
-		return uploadSuccess;
-	}
-
-	public void setUploadSuccess(boolean uploadSuccess) {
-		this.uploadSuccess = uploadSuccess;
-	}
-
 	@Override
 	public String toString() {
 		return "PlayingProgress [id=" + id + ", accountId=" + accountId + ", mediaId=" + mediaId + ", episodeId="
