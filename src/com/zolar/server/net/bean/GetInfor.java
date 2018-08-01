34a9d85438366ff6662922a435a2b552417a0d00
zongh <zongh@LAPTOP-8E1U9OLG>
2018-08-01 13:55:37 +0800

    id改为infor_id

--- src/com/zolar/server/net/bean/GetInfor.java
+++ src/com/zolar/server/net/bean/GetInfor.java
@@ -2,40 +2,47 @@ package com.zolar.server.net.bean;
 
 public class GetInfor {
 
-	private Integer id;			//用户id
-	private Integer account_id;		//机器id
+	private Integer infor_id; // 用户id
+	private Integer account_id; // 机器id
 	private String telephone;
 	private String introduce;
-	public Integer getId() {
-		return id;
+
+	public Integer getInfor_id() {
+		return infor_id;
 	}
-	public void setId(Integer id) {
-		this.id = id;
+
+	public void setInfor_id(Integer infor_id) {
+		this.infor_id = infor_id;
 	}
+
 	public Integer getAccount_id() {
 		return account_id;
 	}
+
 	public void setAccount_id(Integer account_id) {
 		this.account_id = account_id;
 	}
+
 	public String getTelephone() {
 		return telephone;
 	}
+
 	public void setTelephone(String telephone) {
 		this.telephone = telephone;
 	}
+
 	public String getIntroduce() {
 		return introduce;
 	}
+
 	public void setIntroduce(String introduce) {
 		this.introduce = introduce;
 	}
-	
+
 	@Override
 	public String toString() {
-		return "GetInforResponse [id=" + id + ", account_id=" + account_id + ", telephone=" + telephone + ", introduce="
-				+ introduce + "]";
+		return "GetInfor [infor_id=" + infor_id + ", account_id=" + account_id + ", telephone=" + telephone
+				+ ", introduce=" + introduce + "]";
 	}
-
 
 }
